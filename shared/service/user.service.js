ac42281c160d6ee791b4c7802e9d1b145ac58664
Romain Farau <romain.farau@gmail.com>
2015-08-10 14:37:11 +0200

    #9 Debut tests

--- shared/service/user.service.js
+++ shared/service/user.service.js
@@ -22,10 +22,24 @@ angular.module('ecMobileApp.shared').service('userService', function($http, $q) 
             .then(function(result){
                 //Vérification de la connexion doit se faire côté serveur
                 //console.log("Connexion service réussi");
+                //console.log("result "+result.data);
+                
                 connected=true;
                 that.infosUser=result.data;
+
+                console.log("result");
+                console.log(result.data);
+                console.log(result.data.nom);
+                /*console.log("InfosUSer");
+                console.log(that.infosUser);
+                console.log("InfosUSer.nom");
+                console.log(that.infosUser.nom);
+                
+                console.log("string"+objet.toString())*/
+                //console.log("nom"+JSON.stringify(that.infosUser.nom));
                 //$rootScope.loggedUser=result.data;
                 sessionStorage.infosUser=result.data;
+                //console.log("Session : "+sessionStorage.infosUser);
             })
             .catch(function(){
                 //console.log("Echec post connexion");
