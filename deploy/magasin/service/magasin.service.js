cd8281167c106abe191a240251a8ed828cab2c18
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-11 12:16:03 +0200

    auto deploy

--- deploy/magasin/service/magasin.service.js
+++ deploy/magasin/service/magasin.service.js
@@ -4,7 +4,7 @@ angular.module('ecMobileApp.magasin')
 
 
 	var apiBouchon = "bouchons/produits";
-	var apiRestUrl = "http://localhost:8082/ecommerce-backend/api";
+	var apiRestUrl = "http://5.196.89.85:9080/ec-backend/api";
 	var api = "http://localhost:3000/magasin";
 
 	var detailsProduit=[];
@@ -21,7 +21,8 @@ angular.module('ecMobileApp.magasin')
 
 		/*TODO : Tester avec un bouchon le renvoie du get et la transmission de result*/
 		getDetailsProduit : function(id){
-			return $http.get(api+"/"+id)
+			/*return $http.get(api+"/"+id)*/
+			return $http.get(apiRestUrl + "/produit/" + id)
 			.then(function (result){
 				return result.data;
 			});
