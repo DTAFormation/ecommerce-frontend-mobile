7320faf4fc6c37670203e970f9e532b512d7e1e5
Rémi Dendooven <remi.dendooven.job@gmail.com>
2015-08-25 11:44:33 +0200

    Debut de l'ecriture d'un directive un peu plus compliqué

--- magasin/directive/magasin.directive.js
+++ magasin/directive/magasin.directive.js
@@ -1,11 +1,1 @@
-angular.module('ecMobileApp.magasin')
-	
-	.directive('myTab',function(){
-		return{
-			replace: true,
-			transclude: true,
-			templateUrl: 'myTab.html',
-
-		}
-
-	})
+magasin.directive.js
