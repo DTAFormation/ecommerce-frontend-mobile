3d21ed93abe2ccfc261c27a0842d2c37d400aaf2
Rémi Dendooven <remi.dendooven.job@gmail.com>
2015-08-25 17:57:50 +0200

    La moitié de la directive à été écrite

--- magasin/directive/magasin.directive.js
+++ magasin/directive/magasin.directive.js
@@ -1,1 +1,13 @@
-magasin.directive.js
+angular.module('ecMobileApp.magasin')
+
+    .directive('ecLigneProduit', function() {
+        return {
+            restrict:'E',
+            templateUrl : 'magasin/directive/ecLigneProduit.tpl.html',
+           
+            link: function(scope, iElement, iAttrs, ctrl) {
+                var produit = scope.$eval(iAttrs.ngProduit);
+                scope.produit = produit;
+            }
+        };
+    });
